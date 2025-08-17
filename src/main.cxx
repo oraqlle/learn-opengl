3b620f9c60f3abb7f3045e7849dbf3fc6c7744ec
oraqlle <41113853+oraqlle@users.noreply.github.com>
2025-08-17 15:13:30 +1000

    ch2: Introduced rendering of tris using Element Buffer Object (EBO)
    This allows the specification of indicies to use to lookup vertex data
    from a VBO instead of duplicating data for overlapping tri edges.

--- src/main.cxx
+++ src/main.cxx
@@ -107,21 +107,33 @@ int main() {
     // ---- Triangle ----
     // clang-format off
     float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
-         0.0f,  0.5f, 0.0f
+         0.5f,  0.5f, 0.0f,  // top right
+         0.5f, -0.5f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  // bottom left
+        -0.5f,  0.5f, 0.0f   // top left
+    };
+
+    unsigned int indices[] = {
+        0, 1, 3,  // first tri
+        1, 2, 3   // second tri
     };
     // clang-format on
 
-    unsigned int VAO = 1;
+    unsigned int VAO = 0;
     unsigned int VBO = 0;
+    unsigned int EBO = 0;
 
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
 
     glBindVertexArray(VAO);
+
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
@@ -137,7 +149,7 @@ int main() {
         glClear(GL_COLOR_BUFFER_BIT);
         glUseProgram(shader_program);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -145,6 +157,7 @@ int main() {
 
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     glDeleteProgram(shader_program);
 
     glfwTerminate();
