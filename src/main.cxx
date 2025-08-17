77fba1759192fd958cdeb73711091788a25704a4
oraqlle <41113853+oraqlle@users.noreply.github.com>
2025-08-17 13:57:50 +1000

    ch2: Can now compile a vertex and fragment shader for hello triange

--- src/main.cxx
+++ src/main.cxx
@@ -38,6 +38,67 @@ int main() {
 
     glViewport(0, 0, 800, 600);
 
+    // clang-format off
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+         0.0f,  0.5f, 0.0f
+    };
+    // clang-format on
+
+    unsigned int VBO = 0;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    const char *vertex_shader_src = R"(
+#version 330 core
+
+layout (location = 0) in vec3 pos;
+
+void main() {
+    gl_Position = vec4(pos, 1.0);
+}
+    )";
+
+    unsigned int vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, &vertex_shader_src, NULL);
+    glCompileShader(vertex_shader);
+
+    int success = 0;
+    char info_log[512] = {0};
+    glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
+
+    if (!success) {
+        glGetShaderInfoLog(vertex_shader, 512, NULL, info_log);
+        std::cerr << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"
+                  << info_log
+                  << "\n";
+    }
+
+    const char *fragment_shader_src = R"(
+#version 330 core
+
+out vec4 frag_colour;
+
+void main() {
+    frag_colour = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+}
+    )";
+
+    unsigned int fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &fragment_shader_src, NULL);
+    glCompileShader(fragment_shader);
+
+    glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
+
+    if (!success) {
+        glGetShaderInfoLog(fragment_shader, 512, NULL, info_log);
+        std::cerr << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n"
+                  << info_log
+                  << "\n";
+    }
+
     while (!glfwWindowShouldClose(window)) {
         process_input(window);
 
