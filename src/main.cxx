db792a1d4b36cd3dc8c3c3f649a222c460c0e3ce
oraqlle <41113853+oraqlle@users.noreply.github.com>
2025-08-17 16:03:47 +1000

    ch3: Setup colour data as vertex input attributes
    Reverted to single triangle vertex buffer without EBO to keep aligned
    with tutorial code.

--- src/main.cxx
+++ src/main.cxx
@@ -11,20 +11,25 @@ const char *vertex_shader_src = R"(
 #version 330 core
 
 layout (location = 0) in vec3 pos;
+layout (location = 1) in vec3 in_colour;
+
+out vec3 colour;
 
 void main() {
     gl_Position = vec4(pos, 1.0);
+    colour = in_colour;
 }
     )";
 
 const char *fragment_shader_src = R"(
 #version 330 core
 
+in vec3 colour;
+
 out vec4 frag_colour;
-uniform vec4 colour;
 
 void main() {
-    frag_colour = colour;
+    frag_colour = vec4(colour, 1.0);
 }
     )";
 
@@ -109,35 +114,31 @@ int main() {
     // ---- Triangle ----
     // clang-format off
     float vertices[] = {
-         0.5f,  0.5f, 0.0f,  // top right
-         0.5f, -0.5f, 0.0f,  // bottom right
-        -0.5f, -0.5f, 0.0f,  // bottom left
-        -0.5f,  0.5f, 0.0f   // top left
-    };
-
-    unsigned int indices[] = {
-        0, 1, 3,  // first tri
-        1, 2, 3   // second tri
+         // position        // colour
+         0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,  // right
+        -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,  // left
+         0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f   // top
     };
     // clang-format on
 
     unsigned int VAO = 0;
     unsigned int VBO = 0;
-    unsigned int EBO = 0;
 
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
 
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+
+    // colour attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     // Compile with -DPOLYGON_MODE to draw in wireframe polygons.
 #ifdef WIREFRAME_MODE
@@ -158,7 +159,7 @@ int main() {
         glUniform4f(vertex_colour_location, 0.0f, green_value, 0.0f, 1.0f);
 
         glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -166,7 +167,6 @@ int main() {
 
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
-    glDeleteBuffers(1, &EBO);
     glDeleteProgram(shader_program);
 
     glfwTerminate();
